82f8126471334569e4faa5a7e3ef40446195ffc3
arjun <arjunbchennithala@gmail.com>
2022-01-22 12:18:30 +0530

    Some more changes

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const {PDFDocument} = PDFLib;
 const input = document.getElementById("name");
-//const notice = document.getElementById("notice");
 async function load() {
-        //notice.innerHTML = " ";
         
         if(input.value == '' || input.value == " ") {
-                //notice.innerHTML = "Your name is empty...<br>Enter name to continue";
                 return;
         }
         const value = input.value;
