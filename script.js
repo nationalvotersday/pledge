86924220f440b9a9e4435f034a67ed40775b43c5
arjunbchennithala <68032142+arjunbchennithala@users.noreply.github.com>
2022-01-25 00:46:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function load() {
         const obj = await fetch(url).then(res=>res.arrayBuffer());
         const pdfdoc = await PDFDocument.load(obj);
         const pages = pdfdoc.getPages();
-        pages[0].moveTo(400-(value.length * 6), 330);
+        pages[0].moveTo(410-(value.length * 6), 330);
         pages[0].drawText(value, {size:30});
         const saver = await pdfdoc.save();
         download(saver, "certificate.pdf", "application/pdf");
